083d10c4520efddc46c2ca8a7dafa46c47275c06
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-05-07 16:23:17 +0100

    data fe translated into test-it

--- script.py
+++ script.py
@@ -2,11 +2,12 @@ from translate import Translator
 
 try:
     with open('./data.txt', mode='r') as my_file:
-        # print(my_file.read())
         text = my_file.read()
         translator = Translator(to_lang='it')
         translation = translator.translate(text)
         print(translation)
+        with open('./test-it.txt', mode='w') as my_file2:
+            my_file2.write(translation)
 except FileNotFoundError as err:
     print('file does not exist')
     raise err
