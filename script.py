e3ae2adcc1c74977fd01ed740956c515349da411
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-05-07 16:13:47 +0100

    translates data text into terminal

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
+from translate import Translator
+
 try:
     with open('./data.txt', mode='r') as my_file:
-        print(my_file.read())
+        # print(my_file.read())
+        text = my_file.read()
+        translator = Translator(to_lang='it')
+        translation = translator.translate(text)
+        print(translation)
 except FileNotFoundError as err:
     print('file does not exist')
     raise err
